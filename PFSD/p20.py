10b8c4fb3ffb160d1b4a39bc617cd9ae01883dac
Deepthi004 <68506465+Deepthi004@users.noreply.github.com>
2021-01-06 20:50:33 +0530

    Add files via upload

---
+++ PFSD/p20.py
@@ -0,0 +1,6 @@
+n=int(input("Enter Range"))
+for i in range(1,n+1):
+    if(i==7):
+        continue
+    else:
+        print(i)
---
+++ PFSD/p21.py
@@ -0,0 +1,5 @@
+n=int(input("Enter Range"))
+for i in range(1,n+1):
+    if(i==10):
+        pass
+    print(i)
